f7557b793c31017c166ba5db8a04ad6145c88ad2
shaojie.wang.a <shaojie.wang.a@17zuoye.com>
2022-09-09 16:07:13 +0800

    feat 空类型 断言

--- src/05-other.ts
+++ src/05-other.ts
@@ -39,7 +39,7 @@ function removeFromDom(dialog: Dialog, element: Element) {
 }
 function closeDialog(dialog: Dialog) {
   if (!('id' in dialog)) return
-
+ let a =  dialog.id
   setTimeout(() => {
 
     removeFromDom(
@@ -49,6 +49,97 @@ function closeDialog(dialog: Dialog) {
   })
 }
 
-type A = 3
+type VisibleDialog = { id: string }
+type DestroyedDialog = {}
+type Dialog1 = VisibleDialog | DestroyedDialog
+
+function closeDialog1(dialog: Dialog1) {
+  if (!('id' in dialog)) return
+
+  setTimeout(() => {
+    removeFromDom(
+      dialog,
+      document.getElementById(dialog.id)!
+    )
+  })
+}
+
+/**
+ * 模拟名义类型
+ */
+
+type CompanyID = string & { readonly brand: unique symbol }
+type OrderID = string & { readonly brand: unique symbol }
+type UserID = string & { readonly brand: unique symbol }
+type ID = CompanyID | OrderID | UserID
+
+function queryForUser(id: UserID) {
+  console.log(id)
+}
+
+function CompanyID(id: string) {
+  return id as CompanyID
+}
+
+function OrderID(id: string) {
+  return id as OrderID
+}
+
+function UserID(id: string) {
+  return id as UserID
+}
+
+let companyId = CompanyID('4k2k5s')
+let orderId = OrderID('9994acc1')
+let userId = UserID('d21b1dbf')
+
+queryForUser(userId)
+
+/**
+ * 安全的扩展原型
+ */
+declare global {
+  interface Array<T> {
+    zip<U>(list: U []): [T, U][]
+  }
+}
+
+Array.prototype.zip = function<T, U>(
+  this: T [],
+  list: U []
+): [T, U] [] {
+  return this.map((v, k) => [v, list[k]])
+}
+
+
+/**
+ * 练习题
+ */
+ type P1 = (a: number | string) => string
+type P2 = (a: string) => string
+
+function clone(fn: P2) {
+  return fn
+}
+
+let p1: P1 = (a) => 'a'
+clone(p1)
+
+
+type O = {
+  a: {
+    b: {
+      c: string
+    }
+  };
+}
+
+type O1 = keyof O
+
+type MyExclude<T, U> = T extends U ? never : T
+
+type MyExclusive<T, U> = MyExclude<T, U> | MyExclude<U, T>
+
+type MyE1 = MyExclusive<1 | 2 | 3, 2 | 3 | 4>
 
 export {} // 使用模块，防止重复命名报错
